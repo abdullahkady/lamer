45804db2013b6814096180fb2b6423b09c0ff499
Abdullah Kady <abdullahkady@gmail.com>
2018-08-06 13:17:44 +0200

    Add basic frontend for testing

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(compress());
 app.use(helmet());
 app.use(cors());
 
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.post('/encode/:bitRate/:inputFile', controllers.encode);
 app.post('/upload', controllers.upload);
 
